a35917615aa7ded8619ad3f284f730477593de98
heilpas <pascalo.heil@gmail.com>
2018-09-22 15:50:08 +0200

    fixed error in firebase cloud functions

--- cloudfunctions/functions/index.js
+++ cloudfunctions/functions/index.js
@@ -14,24 +14,20 @@ admin.initializeApp();
 exports.createUser = functions.firestore
     .document('users/{userId}')
     .onCreate((snap, context) => {
-      // Get an object representing the document
-      // e.g. {'name': 'Marie', 'age': 66}
-
+      const promises=[];
       const user = snap.data();
-      console.log(user);
-      console.log(snap.id);
       var userObject = {
           uid: snap.id,
           displayName : user.userName,
           password: "password",
           email : user.email
       };
-      console.log(userObject);
 
-      admin.auth().createUser(userObject)
+      return admin.auth().createUser(userObject)
         .then(function(userRecord) {
           // See the UserRecord reference doc for the contents of userRecord.
           console.log("Successfully created new user:", userRecord.uid);
+          
         })
         .catch(function(error) {
           console.log("Error creating new user:", error);
